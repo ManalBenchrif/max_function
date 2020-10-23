ed49d5a9a6bfaa29ee5bd9296dd2e24089710b32
Manal BENCHRIF <72361536+ManalBenchrif@users.noreply.github.com>
2020-10-23 14:06:41 +0100

    Add files via upload

---
+++ max_function.py
@@ -0,0 +1,11 @@
+#The max() function returns the item with the highest value max(n1, n2, n3, ...), or the item with the highest value in an iterable.
+max_number=max(1,50,-100)
+print(max_number)
+
+#If the values are strings, an alphabetically comparison is done.Return the name with the highest value, ordered alphabetically:
+max_string=max("manal","asmae","wiam")
+print(max_string)
+
+#if the values is an iterable
+max_list=max([-50,0,70])
+print(max_list)
